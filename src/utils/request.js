6ee8663aa9e20245cbd95fcf330caf24f62f51c7
zhu1001 <772180508@qq.com>
2018-07-16 16:54:15 +0800

    重定向

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
@@ -32,16 +32,24 @@ service.interceptors.request.use(config => {
 // respone interceptor
 service.interceptors.response.use(
   response => {
-    /**
-    * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
-    * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
-    */
-    const res = response.data;
-    if (res.code === "401") {
-      _this.$router.push("/login");
+    if (response.status == 401) {
+      window.location.href = "http://localhost:8080/#/login?redirect_url=" + encodeURIComponent(location.href);
     }
-    return response
+    return response;
   },
+  /**
+  * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
+  * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
+  */
+  //   const res = response.data;
+  //   if (res.code === "200") {
+  //     console.log("成功")
+  //     this.$router.push({
+  //       redirect: '/login'
+  //     });
+  //   }
+  //   return response
+  // },
   //       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
   //       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
   //         MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
