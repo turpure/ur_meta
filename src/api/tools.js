6b2edba21968649b293357f4da85325550a4c0df
turpure <p812816627@163.com>
2018-10-11 15:04:58 +0800

    fix bug

--- src/api/tools.js
+++ src/api/tools.js
@@ -28,3 +28,56 @@ export function compareDown(data, propertyName) { // 降序排序
     }
   }
 }
+
+// 获取日期
+export function getMonthDate(dateType) {
+  const today = new Date()
+  let y = today.getFullYear()
+  let m = today.getMonth() + 1
+
+  if (m < 10) {
+    m = '0' + m
+  }
+  if (dateType === 'thisMonth') {
+    const start = [y, m, '01'].join('-')
+    const end = [y, m, today.getDate()].join('-')
+    return { start: start, end: end }
+  }
+  if (dateType === 'previousMonth') {
+    m = m - 1
+    if (m === 0) {
+      m = 12
+      y = y - 1
+    }
+    const start = [y, m, '01'].join('-')
+    const endDate = (new Date(y, m, 0)).getDate()
+    const end = [y, m, endDate].join('-')
+    return { start: start, end: end }
+  }
+
+  if (dateType === 'lastMonth') {
+    const end = new Date()
+    const start = new Date()
+    start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+    return { start: start, end: end }
+  }
+}
+
+// 判断dateType
+
+export function getDateType(minDate, maxDate) {
+  const thisMonth = getMonthDate('thisMonth')
+  const previousMonth = getMonthDate('previousMonth')
+  const lastMonth = getMonthDate('lastMonth')
+  if (minDate === thisMonth['start'] && thisMonth['end'] === maxDate) {
+    return 0
+  }
+  if (minDate === previousMonth['start'] && previousMonth['end'] === maxDate) {
+    return 2
+  }
+  if (minDate === lastMonth['start'] && lastMonth['end'] === maxDate) {
+    return 1
+  }
+  return 3
+}
+
--- src/views/reports/sell.vue
+++ src/views/reports/sell.vue
@@ -107,7 +107,7 @@ import {
   getSales
 } from '../../api/profit'
 import { isAdmini } from '../../api/api'
-import { compareUp, compareDown } from '../../api/tools'
+import { compareUp, compareDown, getMonthDate, getDateType } from '../../api/tools'
 import FileSaver from 'file-saver'
 import XLSX from 'xlsx'
 export default {
@@ -147,53 +147,22 @@ export default {
           {
             text: '本月',
             onClick(vm) {
-              const end = new Date()
-              const y = end.getFullYear()
-              let m = end.getMonth() + 1
-              if (m < 10) {
-                m = '0' + m
-              }
-              const firstday = y + '-' + m + '-' + '01'
-              const start = new Date()
-              const sy = start.getFullYear()
-              let sm = start.getMonth() + 1
-              const sd = start.getDate()
-              if (sm < 10) {
-                sm = '0' + sm
-              }
-              const sfirstday = sy + '-' + sm + '-' + sd
-              vm.$emit('pick', [firstday, sfirstday])
-              me.condition.dateRangeType = 0
+              const date = getMonthDate('thisMonth')
+              vm.$emit('pick', [date['start'], date['end']])
             }
           },
           {
             text: '上个月',
             onClick(picker) {
-              const nowdays = new Date()
-              let year = nowdays.getFullYear()
-              let month = nowdays.getMonth()
-              if (month === 0) {
-                month = 12
-                year = year - 1
-              }
-              if (month < 10) {
-                month = '0' + month
-              }
-              const firstDay = [year, month, '01'].join('-')
-              const myDate = new Date(year, month, 0)
-              const lastDay = [year, month, myDate.getDate()].join('-')
-              picker.$emit('pick', [firstDay, lastDay])
-              me.condition.dateRangeType = 2
+              const date = getMonthDate('previousMonth')
+              picker.$emit('pick', [date['start'], date['end']])
             }
           },
           {
             text: '最近一个月',
             onClick(picker) {
-              const end = new Date()
-              const start = new Date()
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-              picker.$emit('pick', [start, end])
-              me.condition.dateRangeType = 1
+              const date = getMonthDate('lastMonth')
+              picker.$emit('pick', [date['start'], date['end']])
             }
           }
         ]
@@ -283,6 +252,7 @@ export default {
     onSubmit(form) {
       const myform = JSON.parse(JSON.stringify(form))
       const height = document.getElementById('app').clientHeight
+      myform.dateRangeType = getDateType(myform.dateRange[0], myform.dateRange[1])
       this.tableHeight = height + 210 + 'px'
       this.show2 = true
       const members = this.allMember
