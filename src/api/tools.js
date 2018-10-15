55b82688015cfdc6841e48b72097265416d75d35
turpure <p812816627@163.com>
2018-10-15 10:42:30 +0800

    date formatter

--- src/api/tools.js
+++ src/api/tools.js
@@ -31,6 +31,9 @@ export function compareDown(data, propertyName) { // 降序排序
 
 // 获取日期
 export function getMonthDate(dateType) {
+  function dateFormatter(date) {
+    return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
+  }
   const today = new Date()
   let y = today.getFullYear()
   let m = today.getMonth() + 1
@@ -59,7 +62,7 @@ export function getMonthDate(dateType) {
     const end = new Date()
     const start = new Date()
     start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-    return { start: start, end: end }
+    return { start: dateFormatter(start), end: dateFormatter(end) }
   }
 }
 
