a60cf12e3244d96403c16b15a0d0c4835682cad4
turpure <p812816627@163.com>
2018-10-30 09:27:31 +0800

    opertation departments

--- src/views/reports/trending.vue
+++ src/views/reports/trending.vue
@@ -372,14 +372,7 @@ export default {
     })
     getSection().then(response => {
       const res = response.data.data
-      let index
-      for (let i = 0; i < res.length; i++) {
-        if ((res[i].department).indexOf('供应链') > -1) {
-          index = i
-          res.splice(index, 1)
-        }
-      }
-      this.department = res
+      this.department = res.filter(ele => ele.department !== '供应链' && ele.department !== '采购部')
     })
     getSecDepartment().then(reseponse => {
       const res = reseponse.data.data
