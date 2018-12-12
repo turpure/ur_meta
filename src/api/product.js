ca3717aebcebd4454c6f812a9df8496cafb2a5ee
zhu1001 <772180508@qq.com>
2018-12-12 17:14:04 +0800

    across

---
+++ src/api/product.js
@@ -0,0 +1,50 @@
+import request from '@/utils/request'
+
+export function goodsList(param) {
+  const data = { 'condition': param }
+  return request({
+    url: '/oa-goods/list',
+    method: 'post',
+    data
+  })
+}
+export function goodsCreate(param) {
+  const data = { 'condition': param }
+  return request({
+    url: '/oa-goods/create',
+    method: 'post',
+    data
+  })
+}
+export function goodsUpdate(param) {
+  const data = { 'condition': param }
+  return request({
+    url: '/oa-goods/update',
+    method: 'put',
+    data
+  })
+}
+export function goodsInfo(param) {
+  const data = { 'condition': param }
+  return request({
+    url: '/oa-goods/info',
+    method: 'post',
+    data
+  })
+}
+export function goodsDelete(param) {
+  const data = { 'condition': param }
+  return request({
+    url: '/oa-goods/delete',
+    method: 'delete',
+    data
+  })
+}
+export function goodsClaim(param) {
+  const data = { 'condition': param }
+  return request({
+    url: '/oa-goods/claim',
+    method: 'psot',
+    data
+  })
+}
--- src/views/productDevelop/productRecommend.vue
+++ src/views/productDevelop/productRecommend.vue
@@ -3,7 +3,7 @@
   <el-col :span="24" style="padding:10px;">
     <el-button type="primary" plain @click='dialogVisible=true'>新增产品</el-button>
     <el-button type="primary">批量导入</el-button>
-    <el-button type="danger">批量删除</el-button>
+    <el-button type="danger" @click="delAll">批量删除</el-button>
     <el-button type="success">下载模板</el-button>
     <el-button type="warning">批量审批</el-button>
   </el-col>
@@ -11,31 +11,34 @@
   <el-dialog title='新增' :visible.sync="dialogVisible">
     <el-form :model="addForm" :rules="rules" label-width="100px" class="demo-ruleForm" ref="addForm">
       <el-form-item label="图片" prop='img'>
-        <el-input v-model="addForm.img" placeholder="必填"></el-input>
+        <el-input size="small" v-model="addForm.img" placeholder="必填"></el-input>
       </el-form-item>
       <el-form-item label="主类目" prop='cate'>
-        <el-select v-model="addForm.cate" style="width:100%;">
-          <el-option value="1" label="1"></el-option>
+        <el-select size="small" v-model="addForm.cate" @change="productcategory" style="width:100%;">
+          <el-option v-for="item in cate" :value="item.CategoryName" :key="item.CategoryName"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="子类目" prop='cateChildren'>
-        <el-select v-model="addForm.cateChildren" style="width:100%;">
-          <el-option value="1" label="1"></el-option>
+      <el-form-item label="子类目" prop='subCate'>
+        <el-select size="small" v-model="addForm.subCate" :disabled="disabled" style="width:100%;">
+          <el-option v-for="item in subCate" :value="item.CategoryName" :key="item.CategoryName"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="开发员" prop='cateDevelop'>
-        <el-select v-model="addForm.cateDevelop" style="width:100%;">
-          <el-option value="1" label="1"></el-option>
+      <el-form-item label="开发员" prop='introducer'>
+        <el-select size="small" v-model="addForm.introducer" style="width:100%;">
+          <el-option v-for="item in developer" :value="item.username" :key="item.username"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="供应商链接1" prop='url1'>
-        <el-input v-model="addForm.url1"></el-input>
+      <el-form-item label="供应商链接" prop='vendor1'>
+        <el-input size="small" v-model="addForm.vendor1"></el-input>
       </el-form-item>
-      <el-form-item label="供应商链接2" prop='url2'>
-        <el-input v-model="addForm.url2"></el-input>
+      <el-form-item label="平台参考链接" prop='origin1'>
+        <el-input size="small" v-model="addForm.origin1"></el-input>
       </el-form-item>
-      <el-form-item label="推荐理由" prop='detail'>
-        <el-input v-model="addForm.detail" placeholder="选填"></el-input>
+      <!-- <el-form-item label="推荐开发员" prop='developer'>
+        <el-input v-model="addForm.developer" placeholder="选填"></el-input>
+      </el-form-item> -->
+      <el-form-item label="推荐理由" prop='introReason'>
+        <el-input size="small" v-model="addForm.introReason" placeholder="选填"></el-input>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -43,101 +46,112 @@
       <el-button type="primary" @click="submitForm">确定</el-button>
     </span>
   </el-dialog>
-  <el-table :data="tableData3" style="width: 100%;">
+  <!-- 查看对话框 -->
+  <el-dialog title='查看' :visible.sync="dialogVisibleView">
+    <el-form :model="viewForm" label-width="100px" ref="viewForm">
+      <el-form-item label="图片" prop="img">
+        <span>{{viewForm.img}}</span>
+      </el-form-item>
+      <el-form-item label="主类目" prop="cate">
+        <span>{{viewForm.cate}}</span>
+      </el-form-item>
+      <el-form-item label="子类目" prop="subCate">
+        <span>{{viewForm.subCate}}</span>
+      </el-form-item>
+      <el-form-item label="供应商链接" prop="vendor1">
+        <span>{{viewForm.vendor1}}</span>
+      </el-form-item>
+      <el-form-item label="平台参考链接" prop="origin1">
+        <span>{{viewForm.origin1}}</span>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+  <!-- 更新对话框 -->
+  <el-dialog title='更新' :visible.sync="dialogVisibleEdit">
+  </el-dialog>
+  <!-- 认领对话框 -->
+  <el-dialog title='认领' :visible.sync="dialogVisibleClaim">
+    <el-select v-model="claimForm.devStatus" placeholder="认领到" size="small" style="width:100%;">
+      <el-option value="正向认领"></el-option>
+      <el-option value="逆向认领"></el-option>
+    </el-select>
+    <span slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="submitClaim">确定</el-button>
+    </span>
+  </el-dialog>
+  <el-table :data="tableData" @selection-change="selsChange" style="width: 100%;">
     <el-table-column type="selection" header-align="center">
     </el-table-column>
     <el-table-column type="index" header-align="center">
     </el-table-column>
-    <el-table-column prop="" label="操作" header-align="center" width="90">
+    <el-table-column label="操作" header-align="center" width="100">
       <template slot-scope="scope">
         <el-tooltip content="查看">
-          <el-button type="text">
+          <el-button type="text" @click="view(scope.$index, scope.row)">
             <i class="el-icon-view"></i>
           </el-button>
         </el-tooltip>
         <el-tooltip content="更新">
-          <i class="el-icon-edit"></i>
+          <el-button type="text" @click="dialogVisibleEdit = true">
+            <i class="el-icon-edit"></i>
+          </el-button>
         </el-tooltip>
         <el-tooltip content="删除">
-          <i class="el-icon-delete"></i>
+          <el-button type="text" @click="del(scope.$index, scope.row)">
+            <i class="el-icon-delete"></i>
+          </el-button>
         </el-tooltip>
         <el-tooltip content="认领">
-          <i class="el-icon-star-on"></i>
+          <el-button type="text" @click="claim(scope.$index, scope.row)">
+            <i class="el-icon-star-on"></i>
+          </el-button>  
         </el-tooltip>
       </template>
     </el-table-column>
-    <el-table-column prop="name" label="图片" header-align="center">
+    <el-table-column prop="img" label="图片" header-align="center">
     </el-table-column>
-    <el-table-column prop="" label="是否备货" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="主类目" header-align="center">
+      <el-table-column prop="cate" :render-header="renderHeader" width='210'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="主类目" header-align="center">
-      <el-table-column :render-header="renderHeader" width='210'>
+    <el-table-column label="子类目" header-align="center">
+      <el-table-column prop="subCate" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="子类目" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="供应商链接" header-align="center">
+      <el-table-column prop="vendor1" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="供应商链接1" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="平台参考链接" header-align="center">
+      <el-table-column prop="origin1" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="平台参考链接1" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="开发员" header-align="center">
+      <el-table-column prop="developer" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="开发编号" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="推荐人" header-align="center">
+      <el-table-column prop="introducer" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="开发员" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="推荐理由" header-align="center">
+      <el-table-column prop="introReason" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="推荐人" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="产品状态" header-align="center">
+      <el-table-column prop="checkStatus" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="推荐理由" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="审批备注" header-align="center">
+      <el-table-column prop="approvalNote" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="产品状态" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="创建时间" header-align="center">
+      <el-table-column prop="createDate" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
-    <el-table-column prop="" label="审批备注" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="创建时间" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="更新时间" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="售价($)" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="预估重量(克)" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="预估利润率(%)" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="预估月销量" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="" label="预估月毛利($)" header-align="center">
-      <el-table-column :render-header="renderHeader" width='150'>
+    <el-table-column label="更新时间" header-align="center">
+      <el-table-column prop="updateDate" :render-header="renderHeader" width='150'>
       </el-table-column>
     </el-table-column>
   </el-table>
@@ -145,29 +159,67 @@
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page="currentPage4"
-      :page-sizes="[100, 200, 300, 400]"
-      :page-size="100"
+      :current-page="this.condition.page"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="this.condition.pageSize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400">
+      :total="this.total">
     </el-pagination>
   </div>
 </section>
 </template>
 
 <script>
+import { goodsList,goodsCreate, goodsDelete, goodsUpdate, goodsInfo, goodsClaim } from '../../api/product.js'
+import { getMember, getGoodscats } from '../../api/profit'
+
 export default {
   data() {
     return {
-      currentPage4: 1,
+      disabled: true,
+      dialogVisibleClaim: false,
+      dialogVisibleEdit: false,
+      dialogVisibleView: false,
+      condition: {
+        cate: '',
+        introducer: '',
+        checkStatus: '',
+        createDate: [],
+        updateDate: [],
+        subCate: '',
+        vendor1: '',
+        origin1: '',
+        developer: '',
+        introReason: '',
+        approvalNote: '',
+        page: 1,
+        pageSize: 10
+      },
+      total: null,
+      sels: [],
+      developer: [],
+      category: [],
+      cate: [],
+      subCate: [],
       addForm:{
-        img:'',
-        cate:'',
-        cateChildren:'',
-        cateDevelop:'',
-        url1:'',
-        url2:'',
-        detail:''
+        img: '',
+        cate: null,
+        subCate: null,
+        introducer: '',
+        vendor1: '',
+        origin1: '',
+        developer: '',
+        introReason: ''
+      },
+      viewForm: {
+        nid: null,
+      },
+      delForm: {
+        nid: []
+      },
+      claimForm: {
+        nid: null,
+        devStatus: ''
       },
       rules: {
           img: [
@@ -188,31 +240,99 @@ export default {
       value: '',
       time:[],
       options: ['1','2'],
-      tableData3: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }
-      ]
+      tableData: []
     }
   },
+  mounted() {
+    this.getData()
+    getMember().then(response => {
+      const possessMan = response.data.data
+      this.developer = possessMan.filter(e => e.position === '开发')
+    })
+    getGoodscats().then(response => {
+      this.category = this.cate = response.data.data
+    })
+  },
   methods:{
+    // 查看
+    view(index, row) {
+      this.dialogVisibleView = true
+      this.viewForm.nid = row.nid
+      goodsInfo(this.viewForm).then(res => {
+        this.viewForm = res.data.data
+      })
+    },
+    // 认领
+    claim(index, row) {
+      this.dialogVisibleClaim = true
+      this.claimForm.nid = row.nid
+    },
+    submitClaim() {
+      console.log(this.claimForm.devStatus,this.claimForm.nid)
+      goodsClaim(this.claimForm).then(res => {})
+    },
+    // 删除
+    selsChange(sels) {
+      this.sels = sels
+    },
+    del(index, row) {
+      console.log(row.nid)
+      this.$confirm('确定删除该条记录？', '提示', { type: 'warning' }).then(() => {
+        this.delForm.nid = [row.nid]
+        goodsDelete(this.delForm).then(res => {
+          this.tableData.splice(index, 1)
+        })
+      })
+    },
+    delAll() {
+      this.delForm.nid = this.sels.map(e => e.nid)
+      this.$confirm('确定删除所选记录？', '提示', { type: 'warning' }).then(() => {
+        goodsDelete(this.delForm).then(res => {
+          this.getData()
+        })
+      })
+    },
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`)
+      this.condition.pageSize = val
+      this.getData()
     },
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`)
+      this.condition.page = val
+      this.getData()
+    },
+    // 子类目
+    productcategory() {
+      if (this.addForm.cate !== '') {
+        this.disabled = false
+        const val = this.addForm.cate
+        const res = this.category
+        this.subCate = res.filter(e => e.CategoryParentName === val)
+      } else if (this.addForm.cate === '') {
+        this.disabled = true
+      }
+    },
+    getData() {
+      goodsList(this.condition).then(res => {
+        this.tableData = res.data.data.items
+        this.condition.page = res.data.data._meta.currentPage
+        this.condition.pageSize = res.data.data._meta.perPage
+        this.total = res.data.data._meta.totalCount
+      })
     },
     submitForm(){
       this.$refs.addForm.validate((valid)=>{
         if(valid){
-          alert('submit')
-          this.dialogVisible=false
+          goodsCreate(this.addForm).then(res => {
+            if (res.data.code === 200) {
+              this.$confirm('成功！', '提示', { type: 'success' }).then(() => {
+                this.dialogVisible=false
+              })
+            } else {
+              this.$confirm('错误！', '提示', { type: 'error' }).then(() => {
+                this.dialogVisible=false
+              })
+            }
+          })
         }else{
           console.log('error submit!!')
           return false
@@ -293,5 +413,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.el-button{
+  margin-left: 0px;
+}
 </style>
