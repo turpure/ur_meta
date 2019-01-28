357fa66eb2e77acd1d44dc3d118b9fe57d375db7
zhu1001 <772180508@qq.com>
2019-01-28 09:34:45 +0800

    delete picture

--- src/views/index.vue
+++ src/views/index.vue
@@ -695,8 +695,8 @@
              class="clearfix-list">
           <h2>活动栏</h2>
         </div>
-        <img src="../assets/1.5.jpg"
-             style="height:400px;width:95%;padding:15px;">
+        <!-- <img src="../assets/1.5.jpg"
+             style="height:400px;width:95%;padding:15px;"> -->
       </el-card>
       <el-dialog :visible.sync="dialogVisible">
         <el-form :model="newsDetailList"
