b864fc6e97dd71acd6c3e475507479cb26191e70
turpure <p812816627@163.com>
2019-03-20 14:24:48 +0800

    update

--- src/views/productDevelop/platWish.vue
+++ src/views/productDevelop/platWish.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <el-col style="position:fixed;bottom:0;z-index:1000;background: #f2f2f2;padding-bottom: 10px;padding-top: 5px;border-top: #eee solid 1px; width: 100%">
+    <el-col style="position:fixed;bottom:0; z-index:1000;background: #f2f2f2;padding-bottom: 10px;padding-top: 5px;border-top: #eee solid 1px; width: 100%">
       <el-button type="primary"
                  @click="update" style="margin-left: 480px">更新</el-button>
       <el-dropdown @command="handleCommand"
