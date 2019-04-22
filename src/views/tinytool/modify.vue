f9edf0be4e61a5dcc1f6a20f505119f3ccc34937
shiwei1210 <379171631@qq.com>
2019-04-22 14:39:48 +0800

    m

--- src/views/tinytool/modify.vue
+++ src/views/tinytool/modify.vue
@@ -52,24 +52,30 @@
       <el-col :span="1">
         <span class="searchMod" @click="searchMod">搜索</span>
       </el-col>
+      <el-col :span="1" style='margin-left:15px;'>
+        <span class="searchMod" @click="exportExcel">导出</span>
+      </el-col>
     </el-row>
     <el-table
       :data="tableData"
-      style="width:100%"
+      style="width:100%;"
       v-loading="listLoading"
+      :height="tableHeight"
+      v-scrollBar:slim
+      :max-height="tableHeight"
       element-loading-text="正在加载中..."
     >
+      <el-table-column prop="salesName" label="销售员"></el-table-column>
       <el-table-column prop="sellerUserid" label="账号"></el-table-column>
       <el-table-column prop="itemId" label="itemId"></el-table-column>
-      <el-table-column prop="salesName" label="销售员"></el-table-column>
       <el-table-column prop="parentSku" label="主SKU"></el-table-column>
       <el-table-column prop="sku" label="子SKU"></el-table-column>
-      <el-table-column prop="updateDate" label="更新时间"></el-table-column>
-      <el-table-column prop="deliveryStorename" label="发货仓库"></el-table-column>
       <el-table-column prop="inventory" label="在线数量"></el-table-column>
       <el-table-column prop="useNum" label="可用库存"></el-table-column>
+      <el-table-column prop="deliveryStorename" label="发货仓库"></el-table-column>
+      <el-table-column prop="updateDate" label="更新时间"></el-table-column>
     </el-table>
-    <el-pagination
+    <!-- <el-pagination
       background
       @size-change="handleSizeChangePic"
       @current-change="handleCurrentChangePic"
@@ -80,7 +86,7 @@
       style="margin-top:10px;"
       v-if="tableData.length!=0"
       :total="this.totalPic"
-    ></el-pagination>
+    ></el-pagination> -->
   </div>
 </template>
 
@@ -90,6 +96,7 @@ export default {
   data() {
     return {
       tableData: [],
+      tableHeight:window.innerHeight -195,
       listLoading: false,
       totalPic: null,
       condition: {
@@ -101,7 +108,7 @@ export default {
         deliveryStorename: null,
         inventory: null,
         useNum: null,
-        pageSize: 20,
+        pageSize: 1000000,
         page: 1
       }
     };
@@ -110,6 +117,40 @@ export default {
     this.searchMod();
   },
   methods: {
+    exportExcel(){
+       this.order = Object.assign({}, this.condition)
+       this.order.pageSize = 100000;
+       getEbayVirtualStore(this.order).then(res => {
+         this.allDataOrder = res.data.data.items
+         const Filename = 'eBay海外仓修改在线数量'
+         const data = this.allDataOrder.map(v => filterVal.map(k => v[k]))
+         const [fileName, fileType, sheetName] = [Filename, 'xls']
+         this.$toExcel({ th, data, fileName, fileType, sheetName })
+       })
+       const th = [
+         '销售员',
+         '账号',
+         'itemId',
+         '主SKU',
+         '子SKU',
+         '在线数量',
+         '可用库存',
+         '发货仓库',
+         '更新时间'
+       ]
+       const filterVal = [
+         'salesName',
+         'sellerUserid',
+         'itemId',
+         'parentSku',
+         'sku',
+         'inventory',
+         'useNum',
+         'deliveryStorename',
+         'updateDate',
+
+       ]
+    },
     handleSizeChangePic(val) {
       this.condition.pageSize = val;
       this.searchMod();
@@ -123,9 +164,9 @@ export default {
       getEbayVirtualStore(this.condition).then(res => {
         this.listLoading = false;
         this.tableData = res.data.data.items;
-        this.totalPic = res.data.data._meta.totalCount;
-        this.condition.pageSize = res.data.data._meta.perPage;
-        this.condition.page = res.data.data._meta.currentPage;
+        // this.totalPic = res.data.data._meta.totalCount;
+        // this.condition.pageSize = res.data.data._meta.perPage;
+        // this.condition.page = res.data.data._meta.currentPage;
       });
     }
   }
