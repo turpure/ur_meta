738b30d8eea59eb0dcd7e7fbe4a903360ef1c7ac
shiwei1210 <379171631@qq.com>
2019-05-22 10:20:38 +0800

    欧洲虚拟仓定价器修改反馈问题

--- src/views/tinytool/ukfic2.vue
+++ src/views/tinytool/ukfic2.vue
@@ -29,25 +29,81 @@
       <el-table-column prop="costprice" label="商品成本（RMB）"></el-table-column>
       <el-table-column prop="Weight" label="商品+包材重（g）"></el-table-column>
     </el-table>
-    <el-table :data="tableData2" style="width:100%;margin-top:20px;">
+    <el-table
+      :data="tableData2"
+      style="width:100%;margin-top:20px;"
+      :header-cell-style="getRowClass"
+    >
       <el-table-column label="根据售价获取毛利率" align="center">
         <el-table-column prop="country" label="国家" width="80" align="center"></el-table-column>
         <el-table-column prop="transport" label="物流方式" align="center"></el-table-column>
-        <el-table-column prop="cost" label="物流费(￥)" width="90" align="center"></el-table-column>
-        <el-table-column prop="price1" label="售价(£)" width="80" align="center"></el-table-column>
-        <el-table-column prop="eFee1" label="eBay交易费(£)" align="center"></el-table-column>
-        <el-table-column prop="pFee1" label="paypal交易费(£)" align="center"></el-table-column>
-        <el-table-column prop="profit1" label="毛利润(£)" width="90" align="center"></el-table-column>
-        <el-table-column prop="profitRmb1" label="毛利润(￥)" width="90" align="center"></el-table-column>
-        <el-table-column prop="rate1" label="毛利率" width="80" align="center"></el-table-column>
+        <el-table-column prop="cost" label="物流费(￥)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.cost | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="price1" label="售价(£)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.price1 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="eFee1" label="eBay交易费(£)" align="center" width="115">
+          <template slot-scope="scope">
+            <span>{{scope.row.eFee1 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="pFee1" label="paypal交易费(£)" align="center" width="110">
+          <template slot-scope="scope">
+            <span>{{scope.row.pFee1 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="profit1" label="毛利润(£)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.profit1 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="profitRmb1" label="毛利润(￥)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.profitRmb1 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="rate1" label="毛利率%" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.rate1 | cutOut}}</span>
+          </template>
+        </el-table-column>
       </el-table-column>
       <el-table-column label="根据毛利率获取售价" align="center">
-        <el-table-column prop="price2" label="售价(£)" width="80" align="center"></el-table-column>
-        <el-table-column prop="eFee2" label="eBay交易费(£)" align="center"></el-table-column>
-        <el-table-column prop="pFee2" label="PayPal交易费(£)" align="center"></el-table-column>
-        <el-table-column prop="profit2" label="毛利润(£)" width="90" align="center"></el-table-column>
-        <el-table-column prop="profitRmb2" label="毛利润(￥)" width="90" align="center"></el-table-column>
-        <el-table-column prop="rate2" label="毛利率" width="80" align="center"></el-table-column>
+        <el-table-column prop="price2" label="售价(£)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.price2 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="eFee2" label="eBay交易费(£)" align="center" width="115">
+          <template slot-scope="scope">
+            <span>{{scope.row.eFee2 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="pFee2" label="PayPal交易费(£)" align="center" width="110">
+          <template slot-scope="scope">
+            <span>{{scope.row.pFee2 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="profit2" label="毛利润(£)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.profit2 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="profitRmb2" label="毛利润(￥)" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.profitRmb2 | cutOut}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="rate2" label="毛利率%" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.rate2 | cutOut}}</span>
+          </template>
+        </el-table-column>
       </el-table-column>
     </el-table>
   </section>
@@ -69,7 +125,35 @@ export default {
       }
     };
   },
+  filters: {
+    cutOut: function(value) {
+      value=Number(value)
+      console.log(value);
+      if (value && Number.isInteger(value)) {
+        console.log(1)
+        return value + ".00";
+      } else {
+        console.log(2)
+        if (value || value != 0) {
+          value = value.toFixed(2);
+          return value;
+        } else if (value == 0) {
+          return "0.00";
+        }else{
+          return value;
+        }
+      }
+    }
+  },
   methods: {
+    getRowClass({ row, column, rowIndex, columnIndex }) {
+      if (rowIndex === 0 && columnIndex === 1) {
+        return "background: #66b1ff;color:#fff";
+      }
+      if (rowIndex === 1 && columnIndex >= 9) {
+        return "background: #66b1ff;color:#fff";
+      }
+    },
     onSubmit() {
       this.listLoading = true;
       getUkfic2(this.condition).then(response => {
