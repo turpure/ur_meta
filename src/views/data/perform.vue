137d71c88a9806e08df18199e6c4f804c4bbeaa8
shiwei1210 <379171631@qq.com>
2019-05-24 10:49:21 +0800

    m

--- src/views/data/perform.vue
+++ src/views/data/perform.vue
@@ -659,11 +659,9 @@ export default {
               series.push(sery)
             })
             this.option.legend.data = lineName
-            console.log(lineName)
             this.option.xAxis[0].data = date
             this.option.series = serie
             this.options.legend.data = lineNames
-            console.log(lineNames)
             this.options.xAxis[0].data = dates
             this.options.series = series
             const _this = this
