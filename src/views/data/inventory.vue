c71823aa449a63c3c8fc44bc13a0908420f4a210
shiwei1210 <379171631@qq.com>
2019-06-18 11:09:34 +0800

    库存情况

--- src/views/data/inventory.vue
+++ src/views/data/inventory.vue
@@ -2,7 +2,9 @@
   <!-- <div>采购毛利润报表</div>     -->
   <div class="toolbar">
     <el-col :span="24" style="padding:10px 20px;">
-      <el-button @click="exportExcel" type="primary">导出表格</el-button>
+      <el-button @click="exportExcel" type="primary">导出总表</el-button>
+      <el-button @click="exportExcelMx" type="success">导出状态明细表</el-button>
+      <el-button @click="exportExcelMxKf" type="warning">导出开发明细表</el-button>
     </el-col>  
     <el-table
       :data="tableData"
@@ -13,6 +15,7 @@
       element-loading-text="正在加载中..."
       @sort-change="sortNumber"
       style="width: 100%"
+      :height="tableHeight"
     >
       <el-table-column prop="storeName" label="仓库" :formatter="empty"></el-table-column>
       <el-table-column
@@ -71,14 +74,14 @@
         :formatter="empty"
         sortable="custom"
       ></el-table-column>
-      <el-table-column label="状态明细" :formatter="empty" width="100">
+      <el-table-column label="状态明细" :formatter="empty" width="90">
         <template slot-scope="scope">
           <el-button type="text" @click="view1(scope.$index, scope.row)">
             <i class="iconfont icon-yulan"></i>
           </el-button>
         </template>
       </el-table-column>
-       <el-table-column label="开发明细" :formatter="empty" width="100">
+       <el-table-column label="开发明细" :formatter="empty" width="90">
         <template slot-scope="scope">
           <el-button type="text" @click="view2(scope.$index, scope.row)">
             <i class="iconfont icon-yulan"></i>
@@ -87,56 +90,56 @@
       </el-table-column>
     </el-table>
     <el-dialog title="查看状态明细" :visible.sync="dialogTableVisible" width="70%">
-      <el-table :data="viewForm">
+      <el-table :data="viewForm" @sort-change="sortNumberView">
         <el-table-column property="storeName" label="仓库"></el-table-column>
-        <el-table-column property="useNum" label="可用库存数量"></el-table-column>
-        <el-table-column property="costmoney" label="可用库存金额">
+        <el-table-column property="useNum" label="可用库存数量" sortable="custom"></el-table-column>
+        <el-table-column property="costmoney" label="可用库存金额" sortable="custom">
             <template slot-scope="scope">{{scope.row['costmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="notInStore" label="在途数量"></el-table-column>
-        <el-table-column property="notInCostmoney" label="在途金额">
+        <el-table-column property="notInStore" label="在途数量" sortable="custom"></el-table-column>
+        <el-table-column property="notInCostmoney" label="在途金额" sortable="custom">
             <template slot-scope="scope">{{scope.row['notInCostmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="hopeUseNum" label="总库存数量"></el-table-column>
-        <el-table-column property="totalCostmoney" label="总库存金额">
+        <el-table-column property="hopeUseNum" label="总库存数量" sortable="custom"></el-table-column>
+        <el-table-column property="totalCostmoney" label="总库存金额" sortable="custom">
             <template slot-scope="scope">{{scope.row['totalCostmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="30DayCostmoney" label="近30天出库金额">
+        <el-table-column property="30DayCostmoney" label="近30天出库金额" sortable="custom" width="150">
             <template slot-scope="scope">{{scope.row['30DayCostmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="sellDays" label="库存周转(天)"></el-table-column>
+        <el-table-column property="sellDays" label="库存周转(天)" sortable="custom"></el-table-column>
         <el-table-column property="goodsStatus" label="状态"></el-table-column>
       </el-table>
-      <div slot="footer" class="dialog-footer">
+      <!-- <div slot="footer" class="dialog-footer">
         <el-button @click="dialogTableVisible = false">取 消</el-button>
         <el-button type="primary" @click="exportExcelMx()">导出明细</el-button>
-      </div>
+      </div> -->
     </el-dialog>
     <el-dialog title="查看开发明细" :visible.sync="dialogTableVisibleKf" width="70%">
-      <el-table :data="viewFormKf">
+      <el-table :data="viewFormKf" @sort-change="sortNumberViewKf">
         <el-table-column property="storeName" label="仓库"></el-table-column>
         <el-table-column property="salerName" label="开发员"></el-table-column>
-        <el-table-column property="useNum" label="可用库存数量"></el-table-column>
-        <el-table-column property="costmoney" label="可用库存金额">
+        <el-table-column property="useNum" label="可用库存数量" sortable="custom"></el-table-column>
+        <el-table-column property="costmoney" label="可用库存金额" sortable="custom">
           <template slot-scope="scope">{{scope.row['costmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="notInStore" label="在途数量"></el-table-column>
-        <el-table-column property="notInCostmoney" label="在途金额">
+        <el-table-column property="notInStore" label="在途数量" sortable="custom"></el-table-column>
+        <el-table-column property="notInCostmoney" label="在途金额" sortable="custom">
           <template slot-scope="scope">{{scope.row['notInCostmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="hopeUseNum" label="总库存数量"></el-table-column>
-        <el-table-column property="totalCostmoney" label="总库存金额">
+        <el-table-column property="hopeUseNum" label="总库存数量" sortable="custom"></el-table-column>
+        <el-table-column property="totalCostmoney" label="总库存金额" sortable="custom">
           <template slot-scope="scope">{{scope.row['totalCostmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="30DayCostmoney" label="近30天出库金额">
+        <el-table-column property="30DayCostmoney" label="近30天出库金额" sortable="custom" width="150">
           <template slot-scope="scope">{{scope.row['30DayCostmoney'] | cutOut}}</template>
         </el-table-column>
-        <el-table-column property="sellDays" label="库存周转(天)"></el-table-column>
+        <el-table-column property="sellDays" label="库存周转(天)" sortable="custom"></el-table-column>
       </el-table>
-      <div slot="footer" class="dialog-footer">
+      <!-- <div slot="footer" class="dialog-footer">
         <el-button @click="dialogTableVisibleKf = false">取 消</el-button>
         <el-button type="primary" @click="exportExcelMxKf()">导出明细</el-button>
-      </div>
+      </div> -->
     </el-dialog>
   </div>
 </template>
@@ -148,12 +151,15 @@ import { compareUp, compareDown, getMonthDate } from "../../api/tools";
 export default {
   data() {
     return {
+      tableHeight:window.innerHeight -160,
       tableData: [],
       listLoading: false,
       viewForm:[],
       viewFormKf:[],
       dialogTableVisible:false,
-      dialogTableVisibleKf:false
+      dialogTableVisibleKf:false,
+      viewFormZt:[],
+      viewFormKfQk:[]
     };
   },
   filters: {
@@ -190,6 +196,10 @@ export default {
             sums[index] = "N/A";
           }
         sums[index] = Math.round(sums[index] * 100) / 100;
+        let arr=sums
+        if(index==8){
+          sums[index] = (arr[6]/arr[7]*30).toFixed(2);
+        }
       });
       return sums;
     },
@@ -221,6 +231,22 @@ export default {
         this.tableData = response.data.data;
       });
     },
+    getDataMx(){
+      let viewFormZt = {
+        storeName: '',
+      };
+      getStockStatusDetail(viewFormZt).then(res => {
+        this.viewFormZt = res.data.data;
+      });
+    },
+    getDataMxKf(){
+      let viewFormKfQk = {
+        storeName: '',
+      };
+      getStockDeveloperDetail(viewFormKfQk).then(res => {
+        this.viewFormKfQk = res.data.data;
+      });
+    },
     exportExcelMxKf() {
       const th = [
         "仓库",
@@ -247,7 +273,7 @@ export default {
         "sellDays"
       ];
       const Filename = "库存情况开发明细";
-      const data = this.viewFormKf.map(v => filterVal.map(k => v[k]));
+      const data = this.viewFormKfQk.map(v => filterVal.map(k => v[k]));
       const [fileName, fileType, sheetName] = [Filename, "xls"];
       this.$toExcel({ th, data, fileName, fileType, sheetName });
     },
@@ -277,7 +303,7 @@ export default {
         "goodsStatus"
       ];
       const Filename = "库存情况状态明细";
-      const data = this.viewForm.map(v => filterVal.map(k => v[k]));
+      const data = this.viewFormZt.map(v => filterVal.map(k => v[k]));
       const [fileName, fileType, sheetName] = [Filename, "xls"];
       this.$toExcel({ th, data, fileName, fileType, sheetName });
     },
@@ -322,6 +348,22 @@ export default {
         this.getData();
       }
     },
+    sortNumberView(column, prop, order) {
+      const data = this.viewForm;
+      if (column.order === "descending") {
+        this.viewForm = data.sort(compareDown(data, column.prop));
+      } else{
+        this.viewForm = data.sort(compareUp(data, column.prop));
+      }
+    },
+    sortNumberViewKf(column, prop, order) {
+      const data = this.viewFormKf;
+      if (column.order === "descending") {
+        this.viewFormKf = data.sort(compareDown(data, column.prop));
+      } else{
+        this.viewFormKf = data.sort(compareUp(data, column.prop));
+      }
+    },
     sortNumber1(column, prop, order) {
       const data = this.tableData1;
       if (column.order === "descending") {
@@ -333,6 +375,8 @@ export default {
   },
   mounted() {
     this.getData();
+    this.getDataMx();
+    this.getDataMxKf();
   }
 };
 </script>
