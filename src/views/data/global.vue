ce4b457cde29378a5e8d343d0571b6227ee7e8f3
shiwei1210 <379171631@qq.com>
2020-05-12 10:56:02 +0800

    全球市场分析移位值

---
+++ src/views/data/global.vue
@@ -0,0 +1,367 @@
+<template>
+  <section>
+    <div>
+      <el-col :span="24" class="toolbar tabe" style="padding:15px 25px;">
+        <div class="floet">
+          <div class="floet01">
+            <span>平台</span>
+            <el-select
+              size="small"
+              v-model="condition.plat"
+              clearable
+              style="width:120px;margin-left:10px;"
+              placeholder="平台"
+            >
+              <el-option
+                v-for="(item,index) in plat"
+                :index="index"
+                :key="item.plat"
+                :label="item.plat"
+                :value="item.plat"
+              ></el-option>
+            </el-select>
+          </div>
+          <div class="floet01">
+            <span>仓库</span>
+            <el-select
+              size="small"
+              v-model="condition.warehouse"
+              clearable
+              style="width:120px;margin-left:10px;"
+              placeholder="仓库"
+            >
+              <el-option
+                v-for="(item,index) in store"
+                :index="index"
+                :key="item"
+                :label="item"
+                :value="item"
+              ></el-option>
+            </el-select>
+          </div>
+          <div class="floet01">
+            <span>账号</span>
+            <el-select
+              size="small"
+              v-model="condition.suffix"
+              filterable
+              clearable
+              style="width:160px;margin-left:10px;"
+              placeholder="账号"
+            >
+              <el-option
+                v-for="(item,index) in account"
+                :index="index"
+                :key="item.id"
+                :label="item.store"
+                :value="item.store"
+              ></el-option>
+            </el-select>
+          </div>
+          <div class="floet01">
+            <span>商品编码</span>
+            <el-input placeholder="请输入商品编码" size="small" style="width:135px;margin-left:10px;" v-model="condition.goodsCode" clearable></el-input>
+          </div>
+          <div class="floet01">
+            <span style="color:red">订单时间</span>
+            <el-date-picker
+              size="small"
+              v-model="condition.orderDate"
+              clearable
+              value-format="yyyy-MM-dd"
+              type="daterange"
+              align="right"
+              unlink-panels
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              class="m10 m210"
+              :picker-options="pickerOptions2"
+            ></el-date-picker>
+          </div>
+          <div class="floet01">
+            <el-button size="small" type="primary" @click="getdata()">查询</el-button>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="24" style="margin-top:1px;">
+        <el-col :span="12">
+          <el-card>
+            <div ref="or1" :style="sty" v-loading="listLoading"></div>
+          </el-card>
+        </el-col>
+        <el-col :span="12">
+          <el-card>
+            <div ref="or2" :style="sty" v-loading="listLoading"></div>
+          </el-card>
+        </el-col>
+      </el-col>
+    </div>
+  </section>
+</template>
+<script type="text/ecmascript-6">
+import { APIGlobalMarket } from "../../api/product";
+import { getPlatform,getAccount,getStore } from "../../api/profit";
+import { compareUp, compareDown, getMonthDate,getNextDate } from "../../api/tools";
+export default {
+  data() {
+    return {
+      tableHeightstock: window.innerHeight - 214,
+      options: [],
+      plat:[],
+      store:[],
+      account:[],
+      total: null,
+      developer: [],
+      sty: {
+        width: "100%",
+        height: window.innerHeight - 168 + "px"
+      },
+      listLoading: false,
+      dateType: [{ id: 0, type: "交易时间" }, { id: 1, type: "发货时间" }],
+      condition: {
+        plat: null,
+        suffix: null,
+        orderDate: [],
+        goodsCode:null,
+        warehouse:null
+      },
+      options1: {
+        title: {
+          top:20,  
+          text: "各国销售量占比",
+          x: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          type: "scroll",
+          x: "center",
+          y: "bottom",
+          data: []
+        },
+        toolbox: {
+          top:10,
+          right:10, 
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            magicType: {
+              show: true,
+              type: ["pie", "funnel"]
+            },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        series: [
+          {
+            name: "访问来源",
+            type: "funnel",
+            width: '85%',
+            height: '85%',
+            funnelAlign: 'left',
+            center: ['75%', '25%'],  // for pie
+            data: []
+          }
+        ]
+      },
+      options2: {
+        title: {
+          top:20,  
+          text: "各国销售额占比($)",
+          x: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          type: "scroll",
+          x: "center",
+          y: "bottom",
+          data: []
+        },
+        toolbox: {
+          top:10,
+          right:10,
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            magicType: {
+              show: true,
+              type: ["pie", "funnel"]
+            },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        series: [
+          {
+            name: "访问来源",
+            type: "funnel",
+            width: '85%',
+            height: '85%',
+            funnelAlign: 'left',
+            center: ['75%', '25%'],  // for pie
+            data:[] 
+          }
+        ]
+      },
+      pickerOptions2: {
+        shortcuts: [
+          {
+            text: "本月",
+            onClick(vm) {
+              const date = getMonthDate("thisMonth");
+              vm.$emit("pick", [date["start"], date["end"]]);
+            }
+          },
+          {
+            text: "上个月",
+            onClick(picker) {
+              const date = getMonthDate("previousMonth");
+              picker.$emit("pick", [date["start"], date["end"]]);
+            }
+          },
+          {
+            text: "最近一个月",
+            onClick(picker) {
+              const date = getMonthDate("lastMonth");
+              picker.$emit("pick", [date["start"], date["end"]]);
+            }
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    getdata() {
+      this.listLoading = true;
+      APIGlobalMarket(this.condition).then(response => {
+        this.listLoading = false;
+        var dataArr1=response.data.data
+        var arr1Name=[]
+        var arr1Data=[]
+        for(let i=0;i<dataArr1.length;i++){
+            arr1Name.push(dataArr1[i].CountryName)
+            var obj={
+                value:dataArr1[i].l_qty,
+                name:dataArr1[i].CountryName
+            }
+            arr1Data.push(obj)
+        }
+        this.options1.legend.data = arr1Name;
+        this.options1.series[0].data = arr1Data;
+        let or1 = this.$echarts.init(this.$refs.or1);
+        or1.setOption(this.options1);
+        var dataArr2=response.data.data
+        var arr2Name=[]
+        var arr2Data=[]
+        for(let i=0;i<dataArr2.length;i++){
+            arr2Name.push(dataArr2[i].CountryName)
+            var obj={
+                value:dataArr2[i].l_AMT,
+                name:dataArr2[i].CountryName
+            }
+            arr2Data.push(obj)
+        }
+        this.options2.legend.data = arr2Name;
+        this.options2.series[0].data = arr2Data;
+        let or2 = this.$echarts.init(this.$refs.or2);
+        or2.setOption(this.options2);
+      });
+    }
+  },
+  mounted() {
+    getPlatform().then(response => {
+      this.plat = response.data.data;
+    });
+    getAccount().then(response => {
+      this.account = response.data.data;
+    });
+    getStore().then(response => {
+      this.store = response.data.data;
+    });
+    var startData = getMonthDate("lastMonth").start;
+    var endData = getMonthDate("lastMonth").end;
+    this.condition.orderDate = [
+      getNextDate(startData, -1),
+      getNextDate(endData, -1)
+    ];
+    // this.getdata();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.clasRed {
+  color: #c55 !important;
+}
+.clasGreen {
+  color: #0e9a00;
+}
+.classa {
+  color: #337ab7;
+  text-decoration: none;
+}
+.clasRed1 {
+  color: #f56c6c;
+  border: rgba(245, 108, 108, 0.2) solid 1px;
+  background: rgba(245, 108, 108, 0.1);
+  width: 65%;
+  margin: auto;
+  line-height: 32px;
+  display: block;
+  border-radius: 5px;
+}
+.clasGreen1 {
+  color: #0e9a00;
+  border-radius: 5px;
+  width: 65%;
+  margin: auto;
+  line-height: 32px;
+  display: block;
+  border: rgba(3, 82, 38, 0.2) solid 1px;
+  background: rgba(33, 170, 95, 0.1);
+}
+.redCu {
+  color: red;
+  font-weight: bold;
+}
+.redblack {
+  color: #606266;
+}
+.saleP {
+  font-size: 14px;
+  margin-top: 5px;
+  display: block;
+  text-align: right;
+  padding-right: 10px;
+}
+.floet {
+  overflow: hidden;
+}
+.floet01 {
+  float: left;
+  margin-right: 20px;
+}
+.el-button {
+  margin-left: 0px;
+}
+.m10 {
+  margin-left: 10px;
+}
+.rid100 {
+  margin-left: 10px;
+}
+.m210 {
+  width: 210px;
+}
+</style>
